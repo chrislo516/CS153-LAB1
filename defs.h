c5aeb78d313511f4a8485263a9352a075b91acb5
Chris <chrislowork@hotmail.com>
2020-04-17 19:27:11 +0000

    Adding int status as an argument in defs.h

--- defs.h
+++ defs.h
@@ -104,7 +104,7 @@ int             pipewrite(struct pipe*, char*, int);
 //PAGEBREAK: 16
 // proc.c
 int             cpuid(void);
-void            exit(void);
+void            exit(int status); // passing status into exit func.
 int             fork(void);
 int             growproc(int);
 int             kill(int);
